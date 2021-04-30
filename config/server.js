84f5eed50424ec0fd6ef1ab3f3d250ad90f8f671
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-04-30 17:43:58 +0300

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '23325fd6e850374c4593e57c428af587'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8da2f5c9-5bf0-4f9f-a35f-01ae2652d24b'
+};
