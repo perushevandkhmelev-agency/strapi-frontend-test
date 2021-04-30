9b7a3d0aa0d16d326a61fec3e846441a8853e20f
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-04-30 19:21:19 +0300

    Add resolver for Mutation

---
+++ api/news-categories/config/schema.graphql.js
@@ -0,0 +1,9 @@
+module.exports = {
+  resolver: {
+    Mutation: {
+      createNewsItem: false,
+      updateNewsItem: false,
+      deleteNewsItem: false,
+    },
+  },
+};
---
+++ api/news-items/config/schema.graphql.js
@@ -0,0 +1,9 @@
+module.exports = {
+  resolver: {
+    Mutation: {
+      createNewsCategory: false,
+      updateNewsCategory: false,
+      deleteNewsCategory: false,
+    },
+  },
+};
